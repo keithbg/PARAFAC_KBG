38cc39ccb4e5a3097027f699b643d5d4ce9923ba
Jordan Wingenroth <jwing421@gmail.com>
2019-04-16 00:29:34 -0700

    I streamlined parafac.r to fit the outline on GDrive: reproducible and readable, albeit with a bit of an improvisational twist at the end

--- Scripts/parafac.r
+++ Scripts/parafac.r
@@ -1,6 +1,9 @@
-
 library(staRdom)
 library(tidyverse)
+
+## this script involves some random subsampling so:
+set.seed(42)
+# this allows me to control when the model recalculates using a new rand. vector
 
 ## load data
 
@@ -11,53 +14,53 @@ eems <- eem_read_csv(path = "../Data/corrected csvs 190311",
                      is_raman_normalized = TRUE,
                      manufacturer = "Horiba")
 
-## outlier removal
+## preliminary PARAFAC run
 
-eems2 <- eem_exclude(eems, list('em' = 541.27,
-                                'ex' = c(246, 249, 252), 
+pf1 <- eem_parafac(eems, comps = 1:10, normalise = TRUE, 
+                    verbose = TRUE, cores = 1)
+
+#1-3 violate Stokes shift so 4 is the bottom limit
+
+#8-10 didn't seem to converge
+
+pf2 <- eem_parafac(eems, comps = 8:10, normalise = TRUE,
+                   maxit = 5000, ctol = 1e-5, cores = 1,
+                   verbose = TRUE)
+
+# 9 and 10 still didn't converge so 8 is upper limit
+
+pf1 <- pf1[4:8]
+
+pf1[5] <- pf2[1]
+
+eempf_compare(pf1)
+
+## core consistency
+
+cc <- unlist(lapply(pf1, function(x) eempf_corcondia(x, eem_interp(eems, type = 0))))
+
+cc
+
+## outlier removal & split-half analysis
+
+### 4C
+
+lev4 <- eempf_leverage(pf1[[1]])
+eempf_leverage_plot(lev4)
+eems4 <- eem_exclude(eems, list('ex' = c(246,249),
+                                'sample' = c('1607_SFPR_EMR_1')))
+sh4 <- splithalf(eem_list = eems4, comps = 4, rand = TRUE, normalise = TRUE)
+splithalf_plot(sh4)
+
+lev5 <- eempf_leverage(pf1[[2]])
+eempf_leverage_plot(lev5)
+eems5 <- eem_exclude(eems, list('ex' = c(246,249, 252),
                                 'sample' = c('1607_SFPR_EMR_1', 
                                              '1605_SFMG_EML_1', 
                                              '1608_SFMG_EML_1',
                                              '1605_REDC_EML_1', 
                                              '1605_SFMD_EML_1', 
                                              '1605_SFCD_EML_1')))
+sh5 <- splithalf(eem_list = eems5, comps = 5, rand = TRUE, normalise = TRUE)
 
-## initial split-half analysis
-
-comps <- c(3:12)
-
-sp_hf <- list()
-
-for (i in 1:length(comps)) {
-  
-  set.seed(42)
-  sp_hf[[i]] <- splithalf(eem_list = eems2, comps = comps[i], rand = TRUE, normalise = TRUE)
-  
-}
-
-## save evidence supporting 4-component model
-
-save(sp_hf, file = 'split-half.rdata')
-
-## comp intensive model
-
-pf_0 <- eem_parafac(eems2, comps = 4, maxit = 10000, normalise = TRUE, 
-                    nstart = 20, ctol = 10^-5, verbose = TRUE)
-
-eempf_compare(pf_0)
-
-save(pf_0, file = 'comp intensive parafac 4-1.RData')
-
-## identify outliers
-
-lev_0 <- eempf_leverage(pf_0[[1]])
-
-eempf_leverage_plot(lev_0)
-
-#I tried re-running the model with the 3 sample outliers removed, but improvement #was negligible. 
-
-# export results to openfluor
-
-pf_rescale <- eempf_rescaleBC(pf_0[[1]])
-
-eempf_openfluor(pf_rescale, file = '4-1-pf.txt')
+sh5on4 <- splithalf(eem_list = eems5, comps = 4, rand = TRUE, normalise = TRUE)
